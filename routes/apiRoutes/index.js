4b1b6646ddf5d5ecc1771e13a53c9b542ec63555
The Prattler <jmichaelsmith@juno.com>
2021-12-27 09:24:05 -0500

    get api/notes functioning

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,11 @@
+const path = require('path');
 const router = require('express').Router();
 const { returnNotes } = require('../../Develop/db/db.json');
 const { notes } = require('../../Develop/db/db.json');
 
-router.get('/api/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = returnNotes(req.query, results);
-  }
-  res.json(results);
+router.get('/notes', (req, res) => {
+  
+  res.json(notes);
 });
 
 module.exports = router;
